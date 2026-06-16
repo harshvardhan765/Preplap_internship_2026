15be5b3e8ab03fb2756b9f6c82d63e37e3c1d793
harsh vardhan shah <205501689+harshvardhan765@users.noreply.github.com>
2026-06-16 17:03:53 +0530

    Initial commit

---
+++ Assignment.py
@@ -0,0 +1,22 @@
+# print("hello")
+# write a program to print name ,age,course name and training batch..!
+# name = "Harsh"
+# age = 20
+# course_name = "BCA"
+# training_batch = "Batch 2024-2027"
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+course_name = input("Enter your course name: ")
+training_batch = input("Enter your training batch: ")
+
+
+
+# Printing the details to the console
+print("====================================")
+print("         STUDENT PROFILE            ")
+print("====================================")
+print(f"Name           : {name}")
+print(f"Age            : {age}")
+print(f"Course         : {course_name}")
+print(f"Training Batch : {training_batch}")
+print("====================================")
