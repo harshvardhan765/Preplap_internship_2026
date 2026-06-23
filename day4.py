f825085d4f668c1b5baeede1afd58f05e8d3c9ba
harsh vardhan shah <205501689+harshvardhan765@users.noreply.github.com>
2026-06-23 15:40:52 +0530

    Updated student details script

---
+++ day4.py
@@ -0,0 +1,29 @@
+#list creating..
+# l = [1,'a','false',89.8]
+# print(l)
+# print(l[0])
+
+
+#indexing
+# tech_stack = ["Python", "Django", "FastAPI", "React"]
+#
+# print(tech_stack[0])   # Output: Python  (First item)
+# print(tech_stack[-1])  # Output: React   (Last item)
+#
+#
+# # slicing
+# numbers = [10, 20, 30, 40, 50, 60]
+#
+# # Extract index 1 up to (but not including) index 4
+# print(numbers[1:4])  # Output: [20, 30, 40]
+#
+# # Grab everything from the start up to index 3
+# print(numbers[:3])   # Output: [10, 20, 30]
+#
+# # Grab everything from index 3 to the very end
+# print(numbers[3:])   # Output: [40, 50, 60]
+
+
+
+# list comp
+
